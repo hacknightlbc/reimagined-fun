c657f2ca45189f4a00f00d0d12074f5b0bba6fc7
poproar <poproar@users.noreply.github.com>
2020-03-05 21:42:49 -0800

    poproar

---
+++ main.js
@@ -0,0 +1,9 @@
+function validateForm() {
+    var x = document.forms["simpleform"]["fname"].value;
+    if (x == "a") {
+      alert("good try");
+      return false;
+    var x = document.getElementById("clue");
+    x.innerHTML = 'The answer'
+    }
+  }
