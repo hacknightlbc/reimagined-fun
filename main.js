3def202f69f0c81ccd01f2960185fab1d04d8b76
poproar <poproar@users.noreply.github.com>
2020-03-06 10:25:34 -0800

    minor fix with js

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-function validateForm() {
-    var x = document.forms["simpleform"]["fname"].value;
-    if (x == "a") {
-      alert("good try");
-      return false;
-    var x = document.getElementById("clue");
-    x.innerHTML = 'The answer'
-    }
-  }
+const queryString = window.location.search;
+console.log(queryString);
+const urlParams = new URLSearchParams(queryString);
+console.log(queryString);
+urlParams.forEach((x, y) =>
+document.forms["simpleform"][y].value = x);
